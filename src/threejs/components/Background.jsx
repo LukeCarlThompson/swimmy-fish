94135598c2fa80e4ad3667d0048853595fec9a35
Luke Carl Thompson <lukecarlthompson@gmail.com>
2021-10-05 00:06:49 +0800

    use instanced mesh for performance reasons

--- src/threejs/components/Background.jsx
+++ src/threejs/components/Background.jsx
@@ -1,24 +1,19 @@
-import React, { useRef } from 'react';
-import { useFrame, useThree } from '@react-three/fiber';
-import { Tube } from '@react-three/drei';
+import React, { useRef, useLayoutEffect } from 'react';
 import * as THREE from 'three';
 
-const Block = props => {
-  console.log('Block');
+const Sphere = props => {
+  console.log('Sphere');
 
   return (
     <>
-      <group>
-        <mesh {...props} scale={1}>
-          <sphereGeometry args={(Math.random() * 0.5 + 0.75) * 8} />
-          <meshLambertMaterial attach="material" color="#275e5c" />
-        </mesh>
-      </group>
+      <sphereGeometry args={(Math.random() * 0.5 + 0.75) * 8} />
+      <meshLambertMaterial attach="material" color="#275e5c" />
     </>
   );
 };
 
 const Background = props => {
+  const mesh = useRef();
   const coordsRef = useRef(
     Array.from(Array(20)).map(item => [
       Math.random() - 0.5,
@@ -26,29 +21,39 @@ const Background = props => {
       Math.random(),
     ])
   );
-  // console.log('coordsRef -->', coordsRef);
+  const dummy = useRef(new THREE.Object3D());
+  const positions = useRef(
+    coordsRef.current.map((item, i) => {
+      const positionX = item[0] * 100;
+      const positionZ = item[1] * -100 + 10;
+      const rotationY = item[2];
+
+      return { positionX, positionZ, rotationY };
+    }),
+    []
+  );
+
+  useLayoutEffect(() => {
+    positions.current.forEach((item, i) => {
+      const { positionX, positionZ, rotationY } = item;
+      const randomHeight = Math.random() * 0.5 + 0.5;
+      dummy.current.position.set(
+        positionX * (Math.random() + 0.5),
+        -15,
+        positionZ * (Math.random() + 0.75)
+      );
+      dummy.current.scale.set(1, randomHeight, 1);
+      dummy.current.updateMatrix();
+      mesh.current.setMatrixAt(i, dummy.current.matrix);
+    });
+    mesh.current.instanceMatrix.needsUpdate = true;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
-    <group>
-      {coordsRef.current.map((item, i) => {
-        const positionX = item[0] * 100;
-        const positionZ = item[1] * -100 + 10;
-        const rotationY = item[2];
-
-        return (
-          <group key={i}>
-            <Block
-              position={[
-                positionX * (Math.random() + 0.5),
-                -15,
-                positionZ * (Math.random() + 0.75),
-              ]}
-              rotation={[rotationY, rotationY, 0]}
-            />
-          </group>
-        );
-      })}
-    </group>
+    <instancedMesh ref={mesh} args={[null, null, positions.current.length]}>
+      <Sphere />
+    </instancedMesh>
   );
 };
 
