2fa311b297636cb31fb1bc0186125ee657a8a5cc
Luke Carl Thompson <lukecarlthompson@gmail.com>
2021-10-09 19:41:32 +0800

    add in a helper to truncate numbers with long decimal values

--- src/threejs/helpers.js
+++ src/threejs/helpers.js
@@ -1,5 +1,11 @@
 const degToRad = degrees => degrees * (Math.PI / 180);
 
+const radToDeg = radians => radians * (180 / Math.PI);
+
+// Sigmoid function returns an eased number range between -1 and 1 but never over or under
 const sigmoid = x => x / (1 + Math.abs(x));
 
-export { sigmoid, degToRad };
+// Removes decimals places but still retiurns a number not a string;
+const truncDec = (int, dec = 2) => Number((int).toFixed(dec));
+
+export { sigmoid, degToRad, radToDeg, truncDec };
